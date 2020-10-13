a45542bca6b857c76aef2ee71409bf39d3737696
Michel Denommee <michel.denommee@gmail.com>
2020-10-13 10:19:31 -0400

    WIP

--- include/evmvc/app.h
+++ include/evmvc/app.h
@@ -42,9 +42,9 @@ class app_t
     : public std::enable_shared_from_this<app_t>
 {
     friend class http_parser;
-    
+
 public:
-    
+
     app_t(struct event_base* ev_base,
         evmvc::app_options&& opts)
         : _init_rtr(false), _init_dirs(false), _status(running_state::stopped),
@@ -58,19 +58,19 @@ public:
 
         // set event_base
         evmvc::global::set_event_base(ev_base);
-        
+
         // init the default logger
         if(_options.use_default_logger){
             std::vector<md::log::sinks::logger_sink> sinks;
-            
+
             auto out_sink = std::make_shared<md::log::sinks::console_sink>(
                 _options.log_console_enable_color
             );
             out_sink->set_level(_options.log_console_level);
             sinks.emplace_back(out_sink);
-            
+
             bfs::create_directories(_options.log_dir);
-            auto file_sink = 
+            auto file_sink =
                 std::make_shared<md::log::sinks::rotating_file_sink>(
                     evmvc::global::ev_base(),
                     (_options.log_dir / "libevmvc.log").c_str(),
@@ -79,7 +79,7 @@ public:
                 );
             file_sink->set_level(_options.log_file_level);
             sinks.emplace_back(file_sink);
-            
+
             _log = std::make_shared<md::log::logger_t>(
                 "/", sinks.begin(), sinks.end()
             );
@@ -95,25 +95,25 @@ public:
                 "/"
             );
     }
-    
+
     ~app_t()
     {
         if(_ev_verif_childs)
             event_free(_ev_verif_childs);
         _ev_verif_childs = nullptr;
-        
+
         evmvc::clear_events();
         _router.reset();
-        
+
         if(this->running())
             this->stop();
-        
+
         // force console buffer flush
         std::cout << std::endl;
     }
-    
+
     app_options& options(){ return _options;}
-    
+
     void replace_logger(md::log::logger l)
     {
         _log = l;
@@ -122,9 +122,9 @@ public:
     {
         return _log;
     }
-    
+
     const std::vector<worker>& workers() const { return _workers;}
-    
+
     running_state status() const
     {
         return _status;
@@ -133,7 +133,7 @@ public:
     bool starting() const { return _status == running_state::starting;}
     bool running() const { return _status == running_state::running;}
     bool stopping() const { return _status == running_state::stopping;}
-    
+
     /*
     * convert a relative path to absolute representation
     * if path starts with '~/' then it's append to base_dir
@@ -157,9 +157,9 @@ public:
         else
             return _options.base_dir / rp;
     }
-    
+
     void set_callbacks(
-        md::callback::async_item_cb<evmvc::process_type> started_cb, 
+        md::callback::async_item_cb<evmvc::process_type> started_cb,
         md::callback::async_item_cb<evmvc::process_type> stopped_cb,
         std::function<void(evmvc::worker)> worker_created_cb,
         std::function<void(evmvc::worker)> worker_deleted_cb)
@@ -169,25 +169,25 @@ public:
         _worker_created_cb = worker_created_cb;
         _worker_deleted_cb = worker_deleted_cb;
     }
-    
+
     void initialize()
     {
         _init_directories();
     }
-    
+
     int start(int argc, char** argv, bool start_event_loop = false)
     {
         _argc = argc;
         _argv = argv;
-        
+
         if(!stopped())
             throw MD_ERR(
                 "app_t must be in stopped state to start listening again"
             );
         _status = running_state::starting;
-        
+
         this->initialize();
-        
+
         std::vector<http_worker> twks;
         for(size_t i = 0; i < _options.worker_count; ++i){
             http_worker w = std::make_shared<evmvc::http_worker_t>(
@@ -195,7 +195,7 @@ public:
             );
             if(_worker_created_cb)
                 _worker_created_cb(w);
-            
+
             int pid = fork();
             if(pid == -1){// fork failed
                 _log->fatal(
@@ -211,7 +211,7 @@ public:
                 twks.emplace_back(w);
             }
         }
-        
+
         // sleep to let time for children to open the unix socket.
         usleep(30000);
         for(auto w : twks){
@@ -226,10 +226,10 @@ public:
                         "unix connect err: {}", errno
                     ));
                     usleep(30000);
-                    
+
                 }else
                     break;
-                
+
                 if(++tryout >= 5){
                     _log->fatal(MD_ERR(
                         "unable to connect to client: {}",
@@ -237,11 +237,11 @@ public:
                     ));
                     return -1;
                 }
-                
+
             }while(w->channel().usock == -1);
             _internal::unix_set_sock_opts(w->channel().usock);
         }
-        
+
         for(auto sc : _options.servers){
             auto s = std::make_shared<master_server_t>(
                 this->shared_from_this(), sc, _log
@@ -249,9 +249,9 @@ public:
             s->start();
             _servers.emplace_back(s);
         }
-        
+
         _status = running_state::running;
-        
+
         // enable child respawn on release build
         // enable master shutdown on debug build
         // TODO: only respawn a limited number of time in a specified period.
@@ -263,8 +263,8 @@ public:
         );
         timeval tv = md::date::ms_to_timeval(1000);
         event_add(_ev_verif_childs, &tv);
-        
-        
+
+
         if(_started_cb)
             set_timeout([self = this->shared_from_this()](auto ew){
                 self->_started_cb(process_type::master,
@@ -275,16 +275,16 @@ public:
                     }
                 });
             }, 0);
-        
+
         if(start_event_loop){
             event_base_loop(global::ev_base(), 0);
             if(running())
                 stop();
         }
-        
+
         return 0;
     }
-    
+
     void stop(md::callback::async_cb cb = nullptr, bool free_ev_base = false)
     {
         if(auto w = evmvc::active_worker()){
@@ -296,10 +296,10 @@ public:
             }
         }
         this->_status = running_state::stopping;
-        
+
         std::clog << "Stopping service" << std::endl;
         _log->info("Stopping service");
-        
+
         for(auto w : _workers){
             if(_worker_deleted_cb)
                 _worker_deleted_cb(w);
@@ -307,20 +307,20 @@ public:
                 w->stop();
             }
         }
-        
+
         //timeout in 30 seconds
         int64_t timeout =
             date::floor<std::chrono::milliseconds>(
                 std::chrono::system_clock::now()
             ).time_since_epoch().count() +
             (int64_t)30000;
-        
+
         bool dead = true;
         do{
             event_base_loop(global::ev_base(), EVLOOP_ONCE);
-            
+
             if(
-                timeout < 
+                timeout <
                 date::floor<std::chrono::milliseconds>(
                     std::chrono::system_clock::now()
                 ).time_since_epoch().count()
@@ -330,7 +330,7 @@ public:
                 }
                 break;
             }
-            
+
             dead = true;
             for(auto w : _workers){
                 int r = kill(w->pid(), 0);
@@ -340,7 +340,7 @@ public:
                     if(p <= 0 || WIFEXITED(wstatus)){
                         continue;
                     }
-                    
+
                     dead = false;
                     break;
                 }else if(r == -1 && errno != ESRCH){
@@ -350,23 +350,23 @@ public:
             }
             usleep(10000);
         }while(!dead);
-        
+
         for(auto w : _workers){
             w->close_channels();
         }
-        
+
         this->_workers.clear();
         this->_servers.clear();
         this->_router.reset();
-        
+
         if(free_ev_base)
             event_base_free(global::ev_base());
-        
+
         std::clog << "Service stopped" << std::endl;
         this->_log->info("Service stopped");
-        
+
         this->_status = running_state::stopped;
-        
+
         if(_stopped_cb)
             _stopped_cb(process_type::master,
             [cb](const md::callback::cb_error& err){
@@ -376,16 +376,16 @@ public:
         else if(cb)
             cb(nullptr);
     }
-    
+
     // ==============
     // == app_t data ==
     // ==============
-    
+
     evmvc::response_data_map app_data() const
     {
         return _app_data;
     }
-    
+
     void clear_data(md::string_view name)
     {
         auto it = _app_data->find(name.to_string());
@@ -393,7 +393,7 @@ public:
             return;
         _app_data->erase(it);
     }
-    
+
     template<typename T>
     void set_data(md::string_view name, T data)
     {
@@ -413,7 +413,7 @@ public:
             );
         }
     }
-    
+
     template<typename T>
     T get_data(md::string_view name, const T& def_val)
     {
@@ -424,7 +424,7 @@ public:
             it->second
         )->value();
     }
-    
+
     template<typename T>
     T get_data(md::string_view name)
     {
@@ -435,11 +435,11 @@ public:
             it->second
         )->value();
     }
-    
+
     // ====================
     // == default router_t ==
     // ====================
-    
+
     router find_router(md::string_view route_t, bool partial_path = false)
     {
         if(!_init_rtr)
@@ -449,13 +449,13 @@ public:
             );
         return _router->find_router(route_t, partial_path);
     }
-    
+
     router use(use_handler_when w, route_handler_cb cb)
     {
         this->_init_router();
         return _router->use(w, cb);
     }
-    
+
     router all(
         const md::string_view& route_path, route_handler_cb cb,
         policies::filter_policy pol = policies::filter_policy())
@@ -526,7 +526,7 @@ public:
         this->_init_router();
         return _router->patch(route_path, cb, pol);
     }
-    
+
     router register_route_handler(
         const md::string_view& verb,
         const md::string_view& route_path,
@@ -536,19 +536,19 @@ public:
         this->_init_router();
         return _router->register_route_handler(verb, route_path, cb, pol);
     }
-    
-    
+
+
     router register_policy(policies::filter_policy pol)
     {
         return _router->register_policy(pol);
     }
-    
+
     router register_policy(
         const md::string_view& route_path, policies::filter_policy pol)
     {
         return _router->register_policy(route_path, pol);
     }
-    
+
     router register_policy(
         evmvc::method method,
         const md::string_view& route_path,
@@ -556,7 +556,7 @@ public:
     {
         return _router->register_policy(method, route_path, pol);
     }
-    
+
     router register_policy(
         const md::string_view& method,
         const md::string_view& route_path,
@@ -564,39 +564,39 @@ public:
     {
         return _router->register_policy(method, route_path, pol);
     }
-    
+
     router register_router(router router_t)
     {
         this->_init_router();
         return _router->register_router(router_t);
     }
-    
+
 private:
-    
+
     static void _verify_child_processes(int /*fd*/, short /*events*/, void* arg)
     {
         app_t* self = (app_t*)arg;
-        
+
         if(self->_status != running_state::running)
             return;
-        
+
         int wstatus = 0;
         pid_t p = waitpid(-1, &wstatus, WNOHANG | WUNTRACED);
-        
+
         if(p <= 0)
             return;
-        
+
         if(WIFSIGNALED(wstatus)){
             int sig = WTERMSIG(wstatus);
             if(sig == SIGINT || sig == SIGKILL){
                 return;
             }
         }
-        
-        #if EVMVC_BUILD_DEBUG
+
+        //#if EVMVC_BUILD_DEBUG
             if(self->_status != running_state::running)
                 return;
-            
+
             // verify if it's a worker process.
             for(
                 auto it = self->_workers.begin();
@@ -612,25 +612,25 @@ private:
                 }
             }
             return;
-        #endif
-        
+        //#endif
+
         // retreive worker instance
         for(auto it = self->_workers.begin(); it != self->_workers.end(); ++it){
             if((*it)->pid() == p){
                 //evmvc::worker w = *it;
                 if(self->_worker_deleted_cb)
                     self->_worker_deleted_cb(*it);
-                
+
                 (*it)->stop(true);
                 self->_workers.erase(it);
-                
+
                 evmvc::worker w = std::make_shared<evmvc::http_worker_t>(
                     self->shared_from_this(), self->_options, self->_log
                 );
-                
+
                 if(self->_worker_created_cb)
                     self->_worker_created_cb(w);
-                
+
                 // restart child proc
                 int pid = fork();
                 if(pid == -1){// fork failed
@@ -645,7 +645,7 @@ private:
                 }else{// in master process
                     w->start(self->_argc, self->_argv, pid);
                 }
-                
+
                 // sleep to let time for children to open the unix socket.
                 usleep(30000);
                 int tryout = 0;
@@ -659,7 +659,7 @@ private:
                         ));
                     }else
                         break;
-                    
+
                     if(++tryout >= 5){
                         self->_log->fatal(MD_ERR(
                             "unable to connect to client: {}",
@@ -669,37 +669,37 @@ private:
                     }
                 }while(w->channel().usock == -1);
                 _internal::unix_set_sock_opts(w->channel().usock);
-                
+
                 self->_workers.emplace_back(w);
-                
+
                 return;
             }
         }
-        
+
         self->_log->warn(MD_ERR(
             "Unknown child has died, PID: '{}'", p
         ));
     }
-    
+
     inline void _init_router()
     {
         if(_init_rtr)
             return;
         _init_rtr = true;
-        
+
         _router =
             std::make_shared<router_t>(
                 this->shared_from_this(), "/"
             );
         //_router->_path = "";
     }
-    
+
     inline void _init_directories()
     {
         if(_init_dirs)
             return;
         _init_dirs = true;
-        
+
         if(bfs::create_directories(_options.base_dir))
             this->_log->info(
                 "Creating base directory '{}'\n", _options.base_dir.c_str()
@@ -720,34 +720,34 @@ private:
             this->_log->info(
                 "Creating run directory '{}'\n", _options.run_dir.c_str()
             );
-        
+
         this->_init_router();
     }
-    
+
     bool _init_rtr;
     bool _init_dirs;
     running_state _status;
     app_options _options;
     router _router;
     md::log::logger _log;
-    
+
     std::vector<evmvc::worker> _workers;
     std::vector<evmvc::master_server> _servers;
-    
-    md::callback::async_item_cb<evmvc::process_type> _started_cb; 
+
+    md::callback::async_item_cb<evmvc::process_type> _started_cb;
     md::callback::async_item_cb<evmvc::process_type> _stopped_cb;
-    
-    std::function<void(evmvc::worker)> _worker_created_cb; 
+
+    std::function<void(evmvc::worker)> _worker_created_cb;
     std::function<void(evmvc::worker)> _worker_deleted_cb;
-    
-    
+
+
     event* _ev_verif_childs;
     evmvc::response_data_map _app_data;
 
     int _argc;
     char** _argv;
-    
-    
+
+
 };//evmvc::app_t
 
 
